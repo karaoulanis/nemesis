57872483b2a65802cd483231d9d87e1771edf9e5
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2007-05-26 15:20:10 +0000

    Initial implementation of 2d, 2/3-node Timoshenko beam.

---
+++ trunk/src/elements/Timoshenko2d.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+*   nemesis. an experimental finite element code.                             *
+*   Copyright (C) 2004-2007 F.E.Karaoulanis [http://www.nemesis-project.org]  *
+*                                                                             *
+*   This program is free software; you can redistribute it and/or modify      *
+*   it under the terms of the GNU General Public License version 2, as        *
+*   published by the Free Software Foundation.                                *
+*                                                                             *
+*   This program is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License along   *
+*   with this program; if not, write to the Free Software Foundation, Inc.,   *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               *
+******************************************************************************/
+
+//*****************************************************************************
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
+// $HeadURL$
+// Author(s): F.E. Karaoulanis (fkar@nemesis-project.org)
+//*****************************************************************************
+
+#ifndef _TIMOSHENKO2D_H
+#define _TIMOSHENKO2D_H
+
+#include <Element.h>
+#include <UniaxialMaterial.h>
+
+class Timoshenko2d: public Element
+{
+protected:
+	double cosX[2];
+	int mySecID;
+	CrossSection* mySection;
+	double L;
+	UniaxialMaterial* myUniMaterial;
+public:
+	Timoshenko2d();
+	Timoshenko2d(int ID,int Node_1,int Node_2,int matID,int secID);	
+	~Timoshenko2d();
+
+	const Matrix& getK();
+    const Matrix& getM();
+	const Vector& getR();
+	const Vector& getRgrad();
+
+	bool checkIfAllows(FEObject* f)		{return true;}
+	void update()						{return;}
+	void commit()						{return;}
+	void recoverStresses();
+	void shapeFunctions(int n,double xi,double &N, double &dN);
+};
+#endif
