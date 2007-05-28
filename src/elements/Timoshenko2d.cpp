1808e571fa7c2089030d3d5129265510f843936d
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2007-05-28 23:47:22 +0000

    Minor enhancements.

--- src/elements/Timoshenko2d.cpp
+++ src/elements/Timoshenko2d.cpp
@@ -57,11 +57,13 @@ Timoshenko2d::Timoshenko2d(int ID,int Node_1,int Node_2,int matID,int secID,int 
 	double b1A=b[1]*A;
 	b[0]= cosX[0]*b0A+cosX[1]*b1A;
 	b[1]=-cosX[1]*b0A+cosX[0]*b1A;
+	gPoints=rule;
+	if(rule!=1&&rule!=2) throw SolverException(9999,"Integration rule should be 1 or 2.");
 }
 Timoshenko2d::Timoshenko2d(int ID,int Node_1,int Node_2,int Node_3,int matID,int secID,int rule)
 :Element(ID)
 {
-	myTag=TAG_ELEM_BEAM_2D_TIMOSHENKO_2;
+	myTag=TAG_ELEM_BEAM_2D_TIMOSHENKO_3;
 	myNodalIDs.resize(3);
 	myNodalIDs[0]=Node_1;
 	myNodalIDs[1]=Node_2;
@@ -87,6 +89,8 @@ Timoshenko2d::Timoshenko2d(int ID,int Node_1,int Node_2,int Node_3,int matID,int
 	double b1A=b[1]*A;
 	b[0]= cosX[0]*b0A+cosX[1]*b1A;
 	b[1]=-cosX[1]*b0A+cosX[0]*b1A;
+	gPoints=rule;
+	if(rule!=2&&rule!=3) throw SolverException(9999,"Integration rule should be 2 or 3.");
 }
 /**
  * Destructor.
@@ -105,7 +109,7 @@ void Timoshenko2d::shapeFunctions(int n,double xi,double &N, double &dN)
 		case 3:
 			if(n==0)		{N =-0.5*xi*(1-xi);	dN=(-1+2*xi)/L;}
 			else if(n==1)	{N = 0.5*xi*(1+xi);	dN=(+1+2*xi)/L;}
-			else			{N = (1+xi)*(1-xi);	dN=+4*xi/L;}
+			else			{N = (1+xi)*(1-xi);	dN=-4*xi/L;}
 			break;
 		default:
 			break;
@@ -127,23 +131,27 @@ const Matrix& Timoshenko2d::getK()
 	double C2=E*J;
 	double C3=a*E/(2*(1+nu))*A;
 
-	double xi=0.;
-	for(unsigned i=0;i<myNodes.size();i++)
+	for(int k=0;k<gPoints;k++)
 	{
-		for(unsigned j=0;j<myNodes.size();j++)
+		double xi=GaussCoords[gPoints][k+1];
+		double dx=GaussWeights[gPoints][k+1]*0.5*L;
+		for(unsigned i=0;i<myNodes.size();i++)
 		{
-			double Ni,Nj,dNi,dNj;
-			this->shapeFunctions(i,xi,Ni,dNi);
-			this->shapeFunctions(j,xi,Nj,dNj);
-			K(3*i+0,3*j+0)=dNi*dNj*(C1*c*c+C3-c*c*C3);
-			K(3*i+0,3*j+1)=c*dNi*dNj*s*(-C3+C1);
-			K(3*i+0,3*j+2)=s*dNi*C3*Nj;
-			K(3*i+1,3*j+0)=c*dNi*dNj*s*(-C3+C1);
-			K(3*i+1,3*j+1)=-dNi*dNj*(-c*c*C3-C1+C1*c*c);
-			K(3*i+1,3*j+2)=-c*dNi*C3*Nj;
-			K(3*i+2,3*j+0)=Ni*C3*dNj*s;
-			K(3*i+2,3*j+1)=-Ni*C3*dNj*c;
-			K(3*i+2,3*j+2)=dNi*C2*dNj+Ni*C3*Nj;
+			for(unsigned j=0;j<myNodes.size();j++)
+			{
+				double Ni,Nj,dNi,dNj;
+				this->shapeFunctions(i,xi,Ni,dNi);
+				this->shapeFunctions(j,xi,Nj,dNj);
+				K(3*i+0,3*j+0)+=(dNi*dNj*(C1*c*c+C3-c*c*C3)	 )*dx;
+				K(3*i+0,3*j+1)+=(c*dNi*dNj*s*(-C3+C1)		 )*dx;
+				K(3*i+0,3*j+2)+=(s*dNi*C3*Nj				 )*dx;
+				K(3*i+1,3*j+0)+=(c*dNi*dNj*s*(-C3+C1)		 )*dx;
+				K(3*i+1,3*j+1)+=(-dNi*dNj*(-c*c*C3-C1+C1*c*c))*dx;
+				K(3*i+1,3*j+2)+=(-c*dNi*C3*Nj				 )*dx;
+				K(3*i+2,3*j+0)+=(Ni*C3*dNj*s				 )*dx;
+				K(3*i+2,3*j+1)+=(-Ni*C3*dNj*c				 )*dx;
+				K(3*i+2,3*j+2)+=(dNi*C2*dNj+Ni*C3*Nj		 )*dx;
+			}
 		}
 	}
 	if(myGroup->isActive())	K*=myGroup->getFacK();
@@ -178,39 +186,41 @@ const Vector& Timoshenko2d::getR()
 	double c=cosX[0];
 	double s=cosX[1];
 	double Ni,dNi;
-	double xi;
 	
-	// Find epsilon 
-	///@todo This should happen to update() for elastoplastic computations
-	static Vector u(3*myNodes.size());
+	Vector u(3*myNodes.size());
 	static Vector epsilon(3);
 	u=this->getDispTrial();
-	xi=0.;
-	epsilon.clear();
-	for(unsigned i=0;i<myNodes.size();i++)
+	R.clear();	// getDispTrial() and R use the same static matrix
+	for(int k=0;k<gPoints;k++)
 	{
-		this->shapeFunctions(i,xi,Ni,dNi);
-		epsilon[0]+=dNi*(c*u[0+3*i]+s*u[1+3*i]);
-		epsilon[1]+=dNi*u[2+3*i];
-		epsilon[2]+=dNi*(-s*u[0+3*i]+c*u[1+3*i])-Ni*u[2+3*i];
+		// Find epsilon 
+		///@todo This should happen to update() for elastoplastic computations
+		double xi=GaussCoords[gPoints][k+1];
+		double dx=GaussWeights[gPoints][k+1]*0.5*L;
+		epsilon.clear();
+		for(unsigned i=0;i<myNodes.size();i++)
+		{
+			this->shapeFunctions(i,xi,Ni,dNi);
+			epsilon[0]+=dNi*(c*u[0+3*i]+s*u[1+3*i]);
+			epsilon[1]+=dNi*u[2+3*i];
+			epsilon[2]+=dNi*(-s*u[0+3*i]+c*u[1+3*i])-Ni*u[2+3*i];
+		}
+		// Find sigma 
+		///@todo This should come from material for elastoplastic computations
+		static Vector sigma(3);
+		sigma[0]=E*A*epsilon[0];
+		sigma[1]=E*J*epsilon[1];
+		sigma[2]=a*E/(2*(1+nu))*A*epsilon[2];
+		// R = Fint
+		for(unsigned i=0;i<myNodes.size();i++)
+		{
+			this->shapeFunctions(i,xi,Ni,dNi);
+			R[3*i+0]+=facS*((dNi*sigma[0])			  )*dx;
+			R[3*i+1]+=facS*((dNi*sigma[2])			  )*dx;
+			R[3*i+2]+=facS*((dNi*sigma[1]-Ni*sigma[2]))*dx;
+		}
 	}
-
-	// Find sigma 
-	///@todo This should come from material for elastoplastic computations
-	static Vector sigma(3);
-	sigma[0]=E*A*epsilon[0];
-	sigma[1]=E*J*epsilon[1];
-	sigma[2]=a*E/(2*(1+nu))*A*epsilon[2];
-
-	// R = Fint - Fext(Gravity) - Fext(Loads)
-	xi=0.;
-	for(unsigned i=0;i<myNodes.size();i++)
-	{
-		this->shapeFunctions(i,xi,Ni,dNi);
-		R[3*i+0]=facS*(dNi*sigma[0]);
-		R[3*i+1]=facS*(dNi*sigma[2]);
-		R[3*i+2]=facS*(dNi*sigma[1]-Ni*sigma[2]);
-	}
+	// R = R - Fext(Gravity)
 	switch(myNodes.size())
 	{
 		case 2:
@@ -230,6 +240,7 @@ const Vector& Timoshenko2d::getR()
 		default:
 			break;
 	}
+	// R = R - Fext(Loads)
 	R-=facP*P;
 	
 	// Local R to global R
@@ -244,5 +255,40 @@ const Vector& Timoshenko2d::getR()
 }
 void Timoshenko2d::recoverStresses()
 {
+	///@todo This might affect nodal stresses.
 	return;
 }
+const double Timoshenko2d::GaussCoords[4][4]=  {{ 0.000000000000000,  // Rule 0
+                                                  0.000000000000000, 
+                                                  0.000000000000000, 
+												  0.000000000000000}, //_______
+ 												{ 0.000000000000000,  // Rule 1
+                                                  0.000000000000000,  // [1][1] 
+                                                  0.000000000000000, 
+												  0.000000000000000}, //_______
+                                                { 0.000000000000000,  // Rule 2
+                                                 -0.577350269189626,  // [2][1]
+                                                 +0.577350269189626,  // [2][2]
+												 0.000000000000000}, //_______
+                                                { 0.000000000000000,  // Rule 3
+                                                 -0.774596669241483,  // [3][1]
+                                                  0.000000000000000,  // [3][2]
+												 +0.774596669241483}};// [3][3]
+ 
+const double Timoshenko2d::GaussWeights[4][4]= {{ 0.000000000000000,  // Rule 0
+                                                  0.000000000000000, 
+                                                  0.000000000000000, 
+                                                  0.000000000000000}, //_______
+                                                { 0.000000000000000,  // Rule 1
+                                                 +2.000000000000000,  // [1][1] 
+                                                  0.000000000000000, 
+												  0.000000000000000}, //_______
+                                                { 0.000000000000000,  // Rule 2
+                                                 +1.000000000000000,  // [2][1]
+                                                 +1.000000000000000,  // [2][1]
+                                                  0.000000000000000}, //_______
+                                                { 0.000000000000000,  // Rule 3
+                                                 +0.555555555555556,  // [3][1]
+                                                 +0.888888888888889,  // [3][2]
+												 +0.555555555555556}};// [3][3]
+ 
