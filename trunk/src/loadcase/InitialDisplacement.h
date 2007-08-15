3528a2674c1876f930ec73896e7d44f634135c15
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2007-08-15 08:14:29 +0000

    Updated license to GPLv3.

--- trunk/src/loadcase/InitialDisplacement.h
+++ trunk/src/loadcase/InitialDisplacement.h
@@ -3,7 +3,7 @@
 *   Copyright (C) 2004-2007 F.E.Karaoulanis [http://www.nemesis-project.org]  *
 *                                                                             *
 *   This program is free software; you can redistribute it and/or modify      *
-*   it under the terms of the GNU General Public License version 2, as        *
+*   it under the terms of the GNU General Public License version 3, as        *
 *   published by the Free Software Foundation.                                *
 *                                                                             *
 *   This program is distributed in the hope that it will be useful,           *
@@ -11,9 +11,8 @@
 *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
 *   GNU General Public License for more details.                              *
 *                                                                             *
-*   You should have received a copy of the GNU General Public License along   *
-*   with this program; if not, write to the Free Software Foundation, Inc.,   *
-*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               *
+*   You should have received a copy of the GNU General Public License         *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
 ******************************************************************************/
 
 //*****************************************************************************
