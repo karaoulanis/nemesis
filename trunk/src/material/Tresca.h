fc3db4a72c329ba3106922c1ad2c415a861206c0
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2007-11-21 10:41:46 +0000

    Added Tresca material law.

---
+++ trunk/src/material/Tresca.h
@@ -0,0 +1,56 @@
+/******************************************************************************
+*   nemesis. an experimental finite element code.                             *
+*   Copyright (C) 2004-2007 F.E.Karaoulanis [http://www.nemesis-project.org]  *
+*                                                                             *
+*   This program is free software; you can redistribute it and/or modify      *
+*   it under the terms of the GNU General Public License version 3, as        *
+*   published by the Free Software Foundation.                                *
+*                                                                             *
+*   This program is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License         *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+******************************************************************************/
+
+//*****************************************************************************
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
+// $HeadURL$
+// Author(s): F.E. Karaoulanis (fkar@nemesis-project.org)
+//*****************************************************************************
+
+#ifndef _TRESCA_H
+#define _TRESCA_H
+
+#include <MultiaxialMaterial.h>
+
+/**
+ * The Tresca Material Class.                                                
+ */
+class Tresca: public MultiaxialMaterial
+{
+protected:
+	MultiaxialMaterial* myElastic;
+	static Matrix C;
+	static Matrix C3;
+	bool plastic;
+	int inaccurate;
+public:
+	Tresca();
+	Tresca(int ID,int elasticID,double sy);
+	~Tresca();
+	
+	MultiaxialMaterial* getClone();
+	void setStrain(const Vector& De);
+	void commit();
+	const Matrix& getC();
+	bool isPlastic();
+
+	// Tracker member functions
+	void track();
+};
+#endif
