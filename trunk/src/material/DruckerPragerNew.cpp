1a67428411e49d339678905c4e1aa7a303dc103d
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2010-10-26 19:34:33 +0000

    File Names according to Google C++ style guide.

---
+++ trunk/src/material/DruckerPragerNew.cpp
@@ -0,0 +1,301 @@
+/******************************************************************************
+*   nemesis. an experimental finite element code.                             *
+*   Copyright (C) 2004-2010 F.E.Karaoulanis [http://www.nemesis-project.org]  *
+*                                                                             *
+*   This program is free software; you can redistribute it and/or modify      *
+*   it under the terms of the GNU General Public License version 3, as        *
+*   published by the Free Software Foundation.                                *
+*                                                                             *
+*   This program is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License         *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+******************************************************************************/
+
+//*****************************************************************************
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
+// $HeadURL$
+// Author(s): F.E. Karaoulanis (fkar@nemesis-project.org)
+//*****************************************************************************
+
+#include "material/DruckerPragerNew.h"
+#include "material/DruckerPragerYS.h"
+#include "material/TensionCutOffYS.h"
+
+Matrix DruckerPragerNew::C(6,6,0.);
+Matrix DruckerPragerNew::C3(3,3,0.);
+
+DruckerPragerNew::DruckerPragerNew()
+{
+}
+DruckerPragerNew::DruckerPragerNew(int ID,int elasticID,double c,double phi,double psi,double Kc,double Kphi,double T)
+:MultiaxialMaterial(ID,0.,0.)
+{
+	// Get the elastic part
+	Material* p=pD->get<Material>(pD->getMaterials(),elasticID);
+	if(p->getTag()!=TAG_MATERIAL_MULTIAXIAL_ELASTIC)
+		throw SException("[nemesis:%d] %s",9999,"Multiaxial elastic material expected.");
+	myElastic=static_cast<MultiaxialMaterial*>(p)->getClone();
+	MatParams[30]=myElastic->getParam(30);
+	MatParams[31]=myElastic->getParam(31);
+
+	// Material properties
+	MatParams[0]=c;
+	MatParams[1]=phi;
+	MatParams[2]=psi;
+	MatParams[3]=Kc;
+	MatParams[4]=Kphi;
+	MatParams[5]=T;
+	
+	plastic=false;
+	inaccurate=0;
+	
+	// Hardening variables
+	aTrial=0.;
+	aConvg=0.;
+
+	fSurfaces.resize(2);
+	fSurfaces[0]=new DruckerPragerYS(c,phi,Kc,Kphi);
+	fSurfaces[1]=new TensionCutOffYS(T);
+	
+	gSurfaces.resize(2);
+	gSurfaces[0]=new DruckerPragerYS(c,psi,Kc,Kphi);
+	gSurfaces[1]=new TensionCutOffYS(T);
+
+	// Material tag
+	myTag=TAG_MATERIAL_DRUCKER_PRAGER;
+}
+DruckerPragerNew::~DruckerPragerNew()
+{
+	delete myElastic;
+}
+MultiaxialMaterial* DruckerPragerNew::getClone()
+{
+	// Material parameters
+	int myID    = this->getID();
+	int elID    = myElastic->getID();
+	double c    = MatParams[ 0];
+	double phi  = MatParams[ 1];
+	double psi  = MatParams[ 2];
+	double Kc   = MatParams[ 3];
+	double Kphi = MatParams[ 4];
+	double T    = MatParams[ 5];
+	// Create clone and return
+	DruckerPragerNew* newClone=new DruckerPragerNew(myID,elID,c,phi,psi,Kc,Kphi,T);
+	return newClone;
+}
+
+
+/**
+ * Update stresses given a total strain increment.
+ * @param De Vector containing total strain increment.
+ */ 
+void DruckerPragerNew::setStrain(const Vector& De)
+{
+
+	// material properties
+	double E = myElastic->getParam(0);
+	double nu= myElastic->getParam(1);
+	double c    = MatParams[ 0];
+	double phi  = MatParams[ 1];
+	double psi  = MatParams[ 2];
+	double Kc   = MatParams[ 3];
+	double Kphi = MatParams[ 4];
+	double T    = MatParams[ 5];
+	
+	// elasticity matrix
+	C3(0,0)=  1/E;	C3(0,1)=-nu/E;	C3(0,2)=-nu/E;
+	C3(1,0)=-nu/E;	C3(1,1)=  1/E;	C3(1,2)=-nu/E;
+	C3(2,0)=-nu/E;	C3(2,1)=-nu/E;	C3(2,2)=  1/E;
+
+	// spectral decomposition
+	Vector s(3),De3(3);
+	Matrix sV(3,3),eV(3,3);
+	aTrial=aConvg;
+	eTrial=eTotal+De;
+	sTrial=sConvg+(this->getC())*De;
+	spectralDecomposition(sTrial,s,sV);
+	Vector eTrial3=C3*s;
+
+	//report(eTrial3,"De",8,3);
+	//report(s,"De",8,3);
+
+	// Find active surfaces
+	vector<int> activeS;
+	for(unsigned i=0;i<2;i++)
+		if(fSurfaces[i]->getf(s,aTrial)>1e-9)
+			activeS.push_back(i);
+	
+
+	//cout<<fSurfaces[0]->getf(s,aTrial)<<' '<<aTrial<<endl;
+	// Elastic (quick return)
+	if(activeS.size()==0)
+		return;
+
+	if(activeS.size()==2)
+		cout<<"2 active"<<endl;
+
+	// Plastic - start iterations ----------------------------------
+	int iter=0;
+	Matrix A;
+	Vector R;
+	Vector x;
+	Vector DLambda(2,0.);
+	while(iter<20)
+	{
+		// increase iteration number
+		++iter;
+		
+		// build system
+		int nA=(int)activeS.size();
+		A.resize(3+nA+1,3+nA+1,0.);
+		R.resize(3+nA+1,0.);
+		x.resize(3+nA+1);
+		A.append(C3,0,0,1.0,1.0);
+
+		R.append(C3*s-eTrial3,0,1.0,0.0);
+		for(int i=0;i<nA;i++)
+		{
+			YS* ys =fSurfaces[activeS[i]];
+			YS* ps =gSurfaces[activeS[i]];
+			double DL=DLambda[activeS[i]];
+			A.append(DL*(ps->getd2fdsds(s,aTrial)),   0,   0, 1., 1.);
+			A.appendCol(ps->getdfds(s,aTrial),        0, 3+i, 1., 0.);
+			A.appendRow(ys->getdfds(s,aTrial),      3+i,   0, 1., 0.);	
+			R.append(DL*(ps->getdfds(s,aTrial)),           0, 1., 1.);
+			R[3+i]=-(ys->getf(s,aTrial));
+			A.appendCol(DL*(ps->getf2dkds(s,aTrial)), 0, 3+nA, 1., 1.);
+			A(3+i,3+nA)+=ys->getdfdk(s,aTrial);
+		}
+		// hardening
+		A.appendRow(DLambda[0]*(gSurfaces[0]->getdfds(s,aTrial)), 3+nA, 0, 1., 0.);
+
+		/////////////////////////////////////////////////////////////
+		A(3+nA  ,3+nA-1)=-EL.geth(gSurfaces[0]->getdfds(s,aTrial));
+		//A(3+nA  ,3+nA-1)=-DLambda[0]*sqrt(2./3.);
+		/////////////////////////////////////////////////////////////
+
+		A(3+nA  ,3+nA  )=1.;
+
+		/////////////////////////////////////////////////////////////
+		R[3+nA]=-aTrial+aConvg+DLambda[0]*(EL.geth(gSurfaces[0]->getdfds(s,aTrial)));
+		//R[3+nA]=-aTrial+aConvg+DLambda[0]*sqrt(2./3.);
+		/////////////////////////////////////////////////////////////
+		
+		// solve
+		//report(A,"A",12,4);
+		//report(R,"R");
+		A.solve(x,R);
+		//report(x,"x");
+
+		// update
+		s[0]+=x[0];
+		s[1]+=x[1];
+		s[2]+=x[2];	
+		for(int i=0;i<nA;i++)
+			DLambda[activeS[i]]+=x[3+i];
+		aTrial+=x[3+nA];
+
+		// check f
+		int check=0;
+		for(int i=0;i<nA;i++)
+		{
+			if(fSurfaces[activeS[i]]->getf(s,aTrial)>1e-9)
+				check++;
+		}
+
+		// check dL<0
+		bool restart=false;
+		if(check==0)
+		{
+			for(int i=0;i<nA;i++)
+			{
+				if(DLambda[activeS[i]]<0.)
+				{
+					cout<<"RESTART "<<nA<<'\n';
+					activeS.erase(activeS.begin()+i);
+					restart=true;
+				}
+			}
+		}
+		if(restart)
+		{
+			DLambda.clear();
+			iter=0;
+		}
+		// check
+		R.append(C3*s-eTrial3,0,1.0,0.0);
+		for(int i=0;i<nA;i++)
+		{
+			YS* ys =fSurfaces[activeS[i]];
+			YS* ps =gSurfaces[activeS[i]];
+			double DL=DLambda[activeS[i]];
+			R.append(DL*(ps->getdfds(s,aTrial)),          0, 1., 1.);
+			R[3+i]=-(ys->getf(s,aTrial));
+		}
+		
+		if(check==0 && restart==false) break;
+	}
+	if(R.twonorm()>1.e-12) report(R,"error");
+	//report(R);
+	//cout<<fSurfaces[0]->getf(s,0.)<<endl;
+	//cout<<fSurfaces[1]->getf(s,0.)<<endl;
+	// coordinate transformation
+	sTrial[0]=s[0]*sV(0,0)*sV(0,0)+s[1]*sV(1,0)*sV(1,0)+s[2]*sV(2,0)*sV(2,0);
+	sTrial[1]=s[0]*sV(0,1)*sV(0,1)+s[1]*sV(1,1)*sV(1,1)+s[2]*sV(2,1)*sV(2,1);
+	sTrial[2]=s[0]*sV(0,2)*sV(0,2)+s[1]*sV(1,2)*sV(1,2)+s[2]*sV(2,2)*sV(2,2);
+	sTrial[3]=s[0]*sV(0,0)*sV(0,1)+s[1]*sV(1,0)*sV(1,1)+s[2]*sV(2,0)*sV(2,1);
+	sTrial[4]=s[0]*sV(0,1)*sV(0,2)+s[1]*sV(1,1)*sV(1,2)+s[2]*sV(2,1)*sV(2,2);
+	sTrial[5]=s[0]*sV(0,0)*sV(0,2)+s[1]*sV(1,0)*sV(1,2)+s[2]*sV(2,0)*sV(2,2);
+
+}
+/**
+ * Commit material state.
+ */
+void DruckerPragerNew::commit()
+{
+	//report(inaccurate);
+	inaccurate=0;
+	eTotal=eTrial; ///@todo
+	sConvg=sTrial;
+	aConvg=aTrial;
+	this->track();
+}
+/**
+ * Get tangent material matrix.
+ * @todo fill it
+ * @return A reference to the tangent material matrix.
+ */
+const Matrix& DruckerPragerNew::getC()
+{
+	return myElastic->getC();
+}
+bool DruckerPragerNew::isPlastic()
+{
+	return plastic;
+}
+/**
+ * Add a record to the tracker.
+ * If \a myTracker pointer is null (no tracker is added) just return.
+ * Otherwise gather info and send them to the tracker.
+ * The domain should be already updated!
+ */
+void DruckerPragerNew::track()
+{
+	if(myTracker==0) return;
+	ostringstream s;
+	s<<"DATA "	<<' ';
+//	s<<"sigm "	<<' '<<sConvg;
+//	s<<"epst "	<<' '<<eTotal;
+//	s<<"epsp "	<<' '<<ePConvg;
+//	s<<"epsv "	<<1020<<' '<<eTotal[0]+eTotal[1]+eTotal[2]<<' ';
+//	s<<"p "	    <<1020<<' '<<sConvg.p()<<' ';
+//	s<<"q "	    <<1020<<' '<<sConvg.q()<<' ';
+//	s<<"END "<<' ';
+	myTracker->track(pD->getLambda(),pD->getTimeCurr(),s.str());
+}
