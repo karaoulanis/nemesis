55ff924a20cbbac6e475d042bfa9ca0124a8e4ae
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2010-10-26 19:45:43 +0000

    File Names according to Google C++ style guide.

---
+++ src/material/tension_cut_off_ys.h
@@ -0,0 +1,20 @@
+
+#ifndef _TENSIONCUTOFFYS_H
+#define _TENSIONCUTOFFYS_H
+
+#include "material/ys.h"
+
+class TensionCutOffYS: public YS
+{
+private:
+	double T;
+public:
+	TensionCutOffYS(double T_);
+	const double getf(const Vector& sigma,const double kappa);
+	const Vector& getdfds(const Vector& sigma,const double kappa);
+	const Matrix& getd2fdsds(const Vector& sigma,const double kappa);
+	const double getdfdk(const Vector& sigma,const double kappa);
+	const Vector& getf2dkds(const Vector& sigma,const double kappa);
+};
+
+#endif
---
+++ src/material/ys.h
@@ -0,0 +1,29 @@
+
+#ifndef _YS_H
+#define _YS_H
+
+#include <vector>
+
+#include "main/nemesis_debug.h"
+#include "numeric/matrix.h"
+
+class YS
+{
+protected:
+	double s1,s2,s3;
+	double I1,J2;
+	Vector a,a1,a2;
+	Matrix da,da2,da11,da22;
+	bool active;
+public:
+	YS();
+	void setSigma(const Vector& s);
+
+	// functions to be overwritten
+	virtual const double getf(const Vector& sigma,const double kappa)=0;
+	virtual const Vector& getdfds(const Vector& sigma,const double kappa)=0;
+	virtual const Matrix& getd2fdsds(const Vector& sigma,const double kappa)=0;
+	virtual const double getdfdk(const Vector& sigma,const double kappa)=0;
+	virtual const Vector& getf2dkds(const Vector& sigma,const double kappa)=0;
+};
+#endif
