77ad4b58d35097cf4b0b2253f304e87f598898e9
Fotios.Karaoulanis <Fotios.Karaoulanis@f2297d5a-b527-0410-9e5e-c5094ff28135>
2010-11-01 13:42:03 +0000

    Updated file comments.

--- trunk/src/material/hoek_brown.h
+++ trunk/src/material/hoek_brown.h
@@ -1,27 +1,27 @@
-/******************************************************************************
-*   nemesis. an experimental finite element code.                             *
-*   Copyright (C) 2004-2010 F.E.Karaoulanis [http://www.nemesis-project.org]  *
-*                                                                             *
-*   This program is free software; you can redistribute it and/or modify      *
-*   it under the terms of the GNU General Public License version 3, as        *
-*   published by the Free Software Foundation.                                *
-*                                                                             *
-*   This program is distributed in the hope that it will be useful,           *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-*   GNU General Public License for more details.                              *
-*                                                                             *
-*   You should have received a copy of the GNU General Public License         *
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
-******************************************************************************/
+/*******************************************************************************
+* nemesis. an experimental finite element code.                                *
+* Copyright (C) 2004-2010 F.E.Karaoulanis [http://www.nemesis-project.org]     *
+*                                                                              *
+* This program is free software; you can redistribute it and/or modify         *
+* it under the terms of the GNU General Public License version 3, as           *
+* published by the Free Software Foundation.                                   *
+*                                                                              *
+* This program is distributed in the hope that it will be useful,              *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
+* GNU General Public License for more details.                                 *
+*                                                                              *
+* You should have received a copy of the GNU General Public License            *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************/
 
-//*****************************************************************************
+// *****************************************************************************
 // $LastChangedDate$
 // $LastChangedRevision$
 // $LastChangedBy$
 // $HeadURL$
 // Author(s): F.E. Karaoulanis (fkar@nemesis-project.org)
-//*****************************************************************************
+// *****************************************************************************
 
 #ifndef NEMESIS_MATERIAL_HOEK_BROWN_H_
 #define NEMESIS_MATERIAL_HOEK_BROWN_H_
