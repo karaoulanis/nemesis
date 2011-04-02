079e182d524ec315d4ffcfedcd4e155af0f32329
Fotios.Karaoulanis@gmail.com <Fotios.Karaoulanis@gmail.com@f2297d5a-b527-0410-9e5e-c5094ff28135>
2011-04-02 10:10:37 +0000

    Initial import of vector.cc vector_test.cc

---
+++ src/numeric/vector.cc
@@ -0,0 +1,66 @@
+/*******************************************************************************
+* nemesis. an experimental finite element code.                                *
+* Copyright (C) 2004-2011 F.E.Karaoulanis [http://www.nemesis-project.org]     *
+*                                                                              *
+* This program is free software; you can redistribute it and/or modify         *
+* it under the terms of the GNU General Public License version 3, as           *
+* published by the Free Software Foundation.                                   *
+*                                                                              *
+* This program is distributed in the hope that it will be useful,              *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
+* GNU General Public License for more details.                                 *
+*                                                                              *
+* You should have received a copy of the GNU General Public License            *
+* along with this program.  If not, see < http://www.gnu.org/licenses/>.       *
+*******************************************************************************/
+
+// *****************************************************************************
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
+// $HeadURL$
+// Author(s): F.E. Karaoulanis (fkar@nemesis-project.org)
+// *****************************************************************************
+
+#include "numeric/vector.h"
+#include "numeric/lapack.h"
+#include "numeric/numeric.h"
+
+const Vector& Vector::eigenvalues() {
+  #ifdef _DEBUG
+  num::check::array_size(size_, 6);
+  #endif
+  int N = 3;
+  char JOBZ='N';
+  char UPLO='L';
+  int LDA = 3;
+  int LWORK = 102;
+  int INFO;
+  static Vector res(3);
+static Vector A(9);
+  static Vector WORK(LWORK);
+  res.clear();
+
+  A[0]=data_[0];
+  A[1]=data_[3];
+  A[2]=data_[5];
+
+  A[3]=0.;
+  A[4]=data_[1];
+  A[5]=data_[4];
+
+  A[6]=0.;
+  A[7]=0.;
+  A[8]=data_[2];
+
+  dsyev(&JOBZ, &UPLO, &N, A.data(), &LDA, res.data(), WORK.data(), &LWORK,
+        &INFO, 1, 1);
+  //  dsyev(&JOBZ, &UPLO, &N, A.data(), &LDA, res.data(), WORK.data(), &LWORK,
+  //        &INFO);
+  //  cout << "Optimal LWORK : "<<WORK[0]<<endl;
+  double d = res[0];
+  res[0]=res[2];
+  res[2]=d;
+  return res;
+}
